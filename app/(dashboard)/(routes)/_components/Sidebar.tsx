3a4ecd18b15f49aaa3a01bd430e5beefc198fb41
shaishguni <gunisguni2007@gmail.com>
2024-05-04 08:48:25 +0545

    created dashbord layout

---
+++ app/(dashboard)/(routes)/_components/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className="h-full border-r flex flex-col ">
+      <div></div>
+    </div>
+  )
+}
+
+export default Sidebar
--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,7 +1,12 @@
+import Sidebar from "./_components/Sidebar";
+
 const DashboardLayout = ({children}:{children:React.ReactNode;})=>{
     return(
         <div className="h-full ">
-            <div className="hidden"></div>{children}</div>
+            <div className="hidden md:flex h-full w-56  flex-col fixed inset-y-0 z-50 ">
+                <Sidebar/>
+            </div>
+            {children}</div>
     )
 
 }
