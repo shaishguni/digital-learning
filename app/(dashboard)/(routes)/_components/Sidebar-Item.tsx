8ae8b8bcdeb6e3e4068a528f8406b52945fd31c2
shaishguni <gunisguni2007@gmail.com>
2024-05-11 07:15:10 +0545

    sidebar layout

--- app/(dashboard)/(routes)/_components/Sidebar-Item.tsx
+++ app/(dashboard)/(routes)/_components/Sidebar-Item.tsx
@@ -1,6 +1,8 @@
 "use client"
+import { cn } from '@/lib/utils'
 import { LucideIcon } from 'lucide-react'
 import { usePathname,useRouter } from 'next/navigation'
+import path from 'path'
 import React from 'react'
 
 interface SidebarItemProps {
@@ -12,15 +14,26 @@ interface SidebarItemProps {
 
 
 const SidebarItem = (
-    {icon:Icon,label:Label,href:Href}:SidebarItemProps
+    {icon:Icon,label,href}:SidebarItemProps
 ) => {
-    const Pathname = usePathname( );
-    const Router = useRouter();
-    // const isActive = (pathname==="/"&)
-
+    const pathname = usePathname( );
+    const router = useRouter();
+    const isActive = ( pathname==='/'&& href==="/")||pathname===href||pathname?.startsWith(`${href}/`)
+    const onClick = ()=>{
+      router.push(href)
+    }
   return (
    
-    <div>SidebarItem</div>
+    <button 
+    onClick={onClick}
+    type='button'
+    className={cn("flex items-center gap-x-2 text-slate-500 text-small font-[500] pl-6 transition-all hover:text-slate-600  hover:bg-slate-300/20",isActive && "text-skay-700 bg-sky-200-20 hover:bg-sky-200/20 hover:text-sky-700 ")}>
+      <div className='flex items-center gap-x-2 py-4'>
+        <Icon size={22} className={cn("text-slate-500 ", isActive&&"text-sky-700")}/>
+      </div>
+      {label}
+     < div className={cn("ml=auto opacity-0 border-0 border-sky-700 h-full translate-all",isActive&&"opacity-100")}/>
+    </button>
   )
 }
 
