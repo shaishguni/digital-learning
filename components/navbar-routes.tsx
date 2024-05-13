c4d1011340668fbaf12a88f7bf803cc3241dec5c
shaishguni <gunisguni2007@gmail.com>
2024-05-13 15:52:33 +0545

    initializing of dynamic layout

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,18 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import { usePathname,useRouter } from "next/navigation"
+
 
 export const NavbarRoutes = ()=>{
+    const pathname = usePathname()
+    const router = useRouter()
+    const isTeacherPage = pathname?.startsWith("/teacher")
+    const isPlayerPage = pathname?.includes("/student")
     return(
         <div className="flex gap-x-2 ml-auto">
             <UserButton/>
+
         </div>
     )
 }
