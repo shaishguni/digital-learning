dc635958ea6bda10a0e3e671051bdada062a9d20
shaishguni <gunisguni2007@gmail.com>
2024-05-13 14:30:23 +0545

    some changes

--- app/(dashboard)/(routes)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/(routes)/_components/mobile-sidebar.tsx
@@ -1,11 +1,14 @@
-import { Divide, Menu } from "lucide-react";
+import { Divide, Menu, Sidebar } from "lucide-react";
 import { Sheet,SheetContent,SheetTrigger } from "@/components/ui/sheet"
 
 export const MobileSidebar = ()=>{
     return(
         <Sheet>
-            <SheetTrigger>
+            <SheetTrigger className="md:hidden pr-4 hover:opacity-75 translate">
                 <Menu/>
+                <SheetContent side="left" className="p-0 bg-white">
+                    <Sidebar/>
+                </SheetContent>
             </SheetTrigger>
         </Sheet>
        
--- app/(dashboard)/(routes)/_components/navbar.tsx
+++ app/(dashboard)/(routes)/_components/navbar.tsx
@@ -1,10 +1,12 @@
+import { NavbarRoutes } from "@/components/navbar-routes"
 import { MobileSidebar } from "./mobile-sidebar"
 import { Sheet,SheetContent,SheetTrigger } from "@/components/ui/sheet"
 
-export const  Navbar= ()=>{
+export const  Navbar = ()=>{
     return(
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
             <MobileSidebar/>
+            <NavbarRoutes/>
         </div>
     )
 }
