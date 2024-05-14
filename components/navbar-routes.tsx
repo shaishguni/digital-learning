d3e844879772eb2b0207a1faff47529b2beec1b7
shaishguni <gunisguni2007@gmail.com>
2024-05-14 21:03:17 +0545

    some cahnges

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,7 +1,11 @@
 "use client"
 
-import { UserButton } from "@clerk/nextjs"
+import { UserButton} from "@clerk/nextjs"
 import { usePathname,useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button"
+
+import Link from "next/link"
+import { LogOut } from "lucide-react"
 
 
 export const NavbarRoutes = ()=>{
@@ -11,8 +15,24 @@ export const NavbarRoutes = ()=>{
     const isPlayerPage = pathname?.includes("/student")
     return(
         <div className="flex gap-x-2 ml-auto">
+            {isTeacherPage||isPlayerPage?(
+                <Button>
+                    <LogOut className="h-4 w-4 mr-2"/>
+                    
+
+                    Exit
+                </Button>
+            ):(
+                <Link href="teacher/courses">
+                <Button>Teacher Mode</Button>
+                </Link>
+            )}
+            
+        
+        
             <UserButton/>
 
         </div> 
     )
+
 }
