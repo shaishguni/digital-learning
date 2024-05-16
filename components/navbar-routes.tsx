bbd199ed5d05c092884f4487c7d7ddc0b359bbfd
shaishguni <gunisguni2007@gmail.com>
2024-05-16 06:03:28 +0545

    some changes

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,38 +1,38 @@
-"use client"
+// "use client"
 
-import { UserButton} from "@clerk/nextjs"
-import { usePathname,useRouter } from "next/navigation"
-import { Button } from "@/components/ui/button"
+// import { UserButton} from "@clerk/nextjs"
+// import { usePathname,useRouter } from "next/navigation"
+// import { Button } from "@/components/ui/button"
 
-import Link from "next/link"
-import { LogOut } from "lucide-react"
+// import Link from "next/link"
+// import { LogOut } from "lucide-react"
 
 
-export const NavbarRoutes = ()=>{
-    const pathname = usePathname()
-    const router = useRouter()
-    const isTeacherPage = pathname?.startsWith("/teacher")
-    const isPlayerPage = pathname?.includes("/student")
-    return(
-        <div className="flex gap-x-2 ml-auto">
-            {isTeacherPage||isPlayerPage?(
-                <Button>
-                    <LogOut className="h-4 w-4 mr-2"/>
+// export const NavbarRoutes = ()=>{
+//     const pathname = usePathname()
+//     const router = useRouter()
+//     const isTeacherPage = pathname?.startsWith("/teacher")
+//     const isPlayerPage = pathname?.includes("/student")
+//     return(
+//         <div className="flex gap-x-2 ml-auto">
+//             {isTeacherPage||isPlayerPage?(
+//                 <Button>
+//                     <LogOut className="h-4 w-4 mr-2"/>
                     
 
-                    Exit
-                </Button>
-            ):(
-                <Link href="teacher/courses">
-                <Button>Teacher Mode</Button>
-                </Link>
-            )}
+//                     Exit
+//                 </Button>
+//             ):(
+//                 <Link href="teacher/courses">
+//                 <Button>Teacher Mode</Button>
+//                 </Link>
+//             )}
             
         
         
-            <UserButton/>
+//             <UserButton/>
 
-        </div> 
-    )
+//         </div> 
+//     )
 
-}
+// }
